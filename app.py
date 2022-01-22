b19cb52ee9849a5b21abd5fe70459089f9d9b7d7
Seokmin Ji <37jr@naver.com>
2022-01-22 14:19:07 +0900

    [mybucketlist] 버킷리스트 생성 api 추가

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://SEOKMINJI:#######@cluster0.xo55a.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.my_bucketlist
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route("/bucket", methods=["POST"])
 def bucket_post():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': 'POST(기록) 연결 완료!'})
+    bucket_receive = request.form['bucket_give']
+    bucket_list = list(db.bucket.find({}, {'_id': False}))
+    count = len(bucket_list) + 1
+
+    doc = {
+        'num': count,
+        'bucket': bucket_receive,
+        'done': 0
+    }
+    db.bucket.insert_one(doc)
+    return jsonify({'msg': '등록 완료!'})
 
 @app.route("/bucket/done", methods=["POST"])
 def bucket_done():
